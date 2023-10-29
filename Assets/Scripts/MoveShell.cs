1a067237748ed74b2b22d2229d361e39e43aafb3
Felipe Ananias <fananiasg@hotmail.com>
2023-10-29 17:10:37 +0000

    Calculating Trajectory

--- Assets/Scripts/MoveShell.cs
+++ Assets/Scripts/MoveShell.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveShell : MonoBehaviour
-{
-    float speed = 1f;
-    // Start is called before the first frame update
+public class MoveShell : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.Translate(0, Time.deltaTime * speed * 0.5f, Time.deltaTime * speed);
+    public float speed = 1.0f;
+
+
+    void Update() {
+
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
 }
